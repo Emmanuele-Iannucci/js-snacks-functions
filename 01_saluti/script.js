4f9a33b4ab85a05bfd37afb909d160d1ca08a683
Emmanuele-Iannucci <emmanueleiannucci@gmail.com>
2025-07-18 18:00:51 +0200

    ex1_arrow

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -6,14 +6,31 @@ const userName = 'Mario';
 // Dichiara la funzione qui.
 
 function salutaUtente(nome) {
-    return 'Ciao' + nome
+    return 'Ciao ' + nome
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-const saluta = salutaUtente (userName)
+let saluta = salutaUtente (userName)
 
 console.log (saluta)
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
+
+/* Aggiungo la funzione arrow con return implicito perché:
+Uso questa sintassi con la arrow function nella forma compatta
+perché la funzione è molto semplice: prende un solo parametro (userName)
+e restituisce direttamente una stringa formata da "Ciao " + il nome passato.
+In questo caso non servono le parentesi graffe né la parola chiave 'return'
+perché JavaScript, quando trova una sola espressione dopo la freccia (=>),
+restituisce automaticamente il suo valore (return implicito).
+Inoltre, non è necessario usare le parentesi tonde intorno al parametro
+perché ce n’è solo uno. Se fossero più parametri, le parentesi sarebbero obbligatorie. */
+
+const salutaUtenteArrow = nome => 'Ciao ' + nome;
+
+// Invoco anche la arrow function
+
+let salutoArrow = salutaUtenteArrow(userName);
+console.log(salutoArrow); // Output: Ciao Mario
