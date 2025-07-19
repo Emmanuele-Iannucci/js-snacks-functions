255769e2725cfddefce458e9ebd1979abe850b20
Emmanuele-Iannucci <emmanueleiannucci@gmail.com>
2025-07-19 09:37:19 +0200

    ex2_revision

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -3,25 +3,27 @@
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Ciclo for fuori dalla funzione: crea l'array di iniziali
-const initials = [];
-
-for (let i = 0; i < names.length; i++) {    
-    const iniziale = names[i][0];                      
-    /*Prendo il nome corrente dell’array (names[i]). 
-    Poi prendo la prima lettera del nome usando [0], cioè il carattere in posizione 0.
-    Salvo questa lettera nella variabile const iniziale */
-    initials.push(iniziale);
-}
 
 // Dichiara la funzione qui 
 // Uso il risultato del ciclo
 
-function stampaIniziali (array) {
-    return array;
+function stampaIniziali(names) {
+
+    const initials = [];
+
+    for (let i = 0; i < names.length; i++) {
+        const iniziale = names[i][0];
+        initials.push(iniziale);
+    }
+
+
+
+
+    return initials;
 }
 // Invoca la funzione qui e stampa il risultato in console
 
-const risultato = stampaIniziali(initials);
+const risultato = stampaIniziali(names);
 console.log(risultato);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
